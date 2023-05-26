76cacb99936e9e8a5352a040f3915a407af2c088
makoto <mako9936@icloud.com>
2023-05-26 20:58:29 +0900

    gtmの設定

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,9 @@ function MyApp({ Component, pageProps }) {
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl
+            + '&gtm_auth=ZVvd9l-6gcbWCr_F0vUZ6A&gtm_preview=env-1&gtm_cookies_win=x';
+            f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer', '${GTM_ID}');
           `,
         }}
